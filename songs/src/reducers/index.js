02fb8cdd59e08d73415d5cca7351d16d0e96d71d
Hassan Ramadan <HassanRamadanEbrahim@gmail.com>
2021-06-13 10:29:54 +0200

    Add Songs

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux'
+
+const songsReducer = () => {
+  return [
+    { title: 'Hello!', duration: '4:55' },
+    { title: 'It is you!', duration: '5:32' },
+    { title: 'How about you!', duration: '3:09' },
+    { title: 'Dear believer, believe in me:)', duration: '5:43' },
+  ]
+}
+
+const selectedSongReducer = (state = null, action) => {
+  if (action.type === 'SELECTED_SONG') {
+    return action.payload
+  }
+  return state
+}
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+})
+
+
+
