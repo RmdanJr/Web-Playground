128051a135a081844b48a489f6650d3f0a502f40
Hassan Ramadan <HassanRamadanEbrahim@gmail.com>
2021-07-10 17:39:48 +0200

    style active navtab

--- todo/src/components/NavBar.tsx
+++ todo/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../styles/NavBar.css'
 
 const NavTab = ({ title }: { title: string }) => {
@@ -10,9 +10,13 @@ const NavBar = () => {
   return (
     <div className='nav-bar'>
       {['All', 'Active', 'Completed'].map((navTab) => (
-        <Link to={`/${navTab}`.toLowerCase()} key={navTab}>
+        <NavLink
+          to={`/${navTab}`.toLowerCase()}
+          key={navTab}
+          activeStyle={{ borderBottom: '2px solid #2f80ed' }}
+        >
           <NavTab title={navTab} />
-        </Link>
+        </NavLink>
       ))}
     </div>
   )
